2485e58e92f56f3f106fd663ec26dc9188a050a0
khayamx <53055109+khayamx@users.noreply.github.com>
2019-10-13 18:42:24 +0100

    added write to value

--- main.cpp
+++ main.cpp
@@ -19,7 +19,19 @@ static uint16_t port;
 int buttonPin = 17; //GPIO17 Pin on the Pi
 bool buttonState = false;  //Used to store the previous state of the button
 int LEDPin = 23;// physical LED
+//terminals and lables/=value dsiplay
+WidgetTerminal terminal(V1);
+//Widget
+BlynkTimer timer;
+void sensorDataSend(void);
+int sensorValue;
 
+/*BLYNK_READ(V1)
+{
+	terminal.println("Hello bitch");
+	terminal.flush();
+
+}*/
 void setup()
 {
     //SETUP BLYNK
@@ -29,37 +41,50 @@ void setup()
     pullUpDnControl (buttonPin, PUD_UP); //Set GPIO17 internal pull up
     pinMode(LEDPin, OUTPUT);//set external LED
 
+    //terminal stuff
+	/*
+    terminal.println(F("Blynk v" BLYNK_VERSION ": Device started"));
+    terminal.println(F("-------------"));
+    terminal.println(F("Type 'Marco' and get a reply, or type"));
+    terminal.println(F("anything else and get it printed back."));
+    terminal.flush();*/
+    timer.setInterval(1000L, sensorDataSend); //timer will run every sec
 }
  
 void loop()
 {
     Blynk.run();
-    //switch LED off
-/*    if (digitalRead(buttonPin) == HIGH){
-        Blynk.virtualWrite(V0, 0);  //Else we set the virtual pin "V0" to a value of 255 (full on)
-        digitalWrite (LEDPin, 0);
+    timer.run();
+}
+void sensorDataSend(){
 
-    }*/
-    if(buttonState != digitalRead(buttonPin)) //check the button state against its last known value, if true:
+	sensorValue = 15;         // reading sensor from analog pin
+  	Blynk.virtualWrite(V2, sensorValue);  // sending sensor value to Blynk app
+}
+
+void button(){
+	
+if(buttonState != digitalRead(buttonPin)) //check the button state against its last known value, i$
     {
-       if(digitalRead(buttonPin) == TRUE) //if true, set the Virtual Pin "V0" to a value of 0 (full off)
+       if(digitalRead(buttonPin) == TRUE) //if true, set the Virtual Pin "V0" to a value of 0 (full of$
         {
             Blynk.virtualWrite(V0, 0);
-	    digitalWrite (LEDPin, 0);
+            digitalWrite (LEDPin, 0);
 
         }
-        else{  
-            Blynk.virtualWrite(V0, 255);  //Else we set the virtual pin "V0" to a value of 255 (full on)
+        else{
+            Blynk.virtualWrite(V0, 255);  //Else we set the virtual pin "V0" to a value of 255 (full o$
             digitalWrite (LEDPin, 1);
 
-	}
-    }   
-    else {}    //if last value = current value, we do nothing. 
-    
-buttonState = digitalRead(buttonPin);  //update the button state. 
+        }
+    }
+    else {}    //if last value = current value, we do nothing.
+
+buttonState = digitalRead(buttonPin);  //update the button state.
+
+
 
 }
- 
  
 int main(int argc, char* argv[])
 {   
@@ -67,8 +92,10 @@ int main(int argc, char* argv[])
     setup();
     digitalWrite(LEDPin,1);
     Blynk.virtualWrite(V0, 1);
+    //terminal.print("hello bitch");
     while(true) {
         loop();
+	button();
     }
  
     return 0;
